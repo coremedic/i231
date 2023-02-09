2be2348a2ca1ef7c9365f4268b7d802c4ea6447f
will <willjcim@gmail.com>
2023-02-09 18:01:18 -0500

    working

--- 03hw/extendedeuclidean.py
+++ 03hw/extendedeuclidean.py
@@ -4,18 +4,25 @@ def extendedGCD(a, b):
         return b, 0, 1
             
     gcd, s1, t1 = extendedGCD(b%a, a)
-    print("\ta: " + str(a) + "\tb: " + str(b) + "\ts: " + str(s1) + "\tt: " + str(t1))
-    print(str(a) + "(" + str(s1) + ")+" + str(b) + "(" + str(t1) + ") = " + str(gcd))
+
+    #print("Pre:\ta:" + str(a) + "\tb:" + str(b) + "\ts:" + str(s1) + "\tt:" + str(t1) + "\tFormula: " + str(a) + "(" + str(s1) + ")+" + str(b) + "(" + str(t1) + ") = " + str(gcd))
+    print("Pre:\ta:" + str(a) + "\tb:" + str(b) + "\ts:" + str(s1) + "\tt:" + str(t1))
+
     s,t = updateCoeff(a,b,s1,t1)
-    print("\ta: " + str(a) + "\tb: " + str(b) + "\ts: " + str(s) + "\tt: " + str(t))
-    print(str(a) + "(" + str(s) + ")+" + str(b) + "(" + str(t) + ") = " + str(gcd) + "\n")
+
+    print("Post:\ta:" + str(a) + "\tb:" + str(b) + "\ts:" + str(s) + "\tt:" + str(t) + "\nFormula: " + str(a) + "(" + str(s) + ")+" + str(b) + "(" + str(t) + ") = " + str(gcd) + "\n")
+
     return gcd, s, t
 
 
 def updateCoeff(a,b,s,t):
-    #print("\t")
-    return (t - (b//a) * s, s) 
+    newT = s
+    newS = (b//a) * s
+    newS = t - newS
+    return (newS, newT) 
 
+def run(a, b):
+    gcd, s, t = extendedGCD(a,b)
+    print("Final:\ta:" + str(a) + "\tb:" + str(b) + "\ts:" + str(s) + "\tt:" + str(t) + "\nFormula: " + str(a) + "(" + str(s) + ")+" + str(b) + "(" + str(t) + ") = " + str(gcd) + "\n")
 
-gcd, s, t = extendedGCD(56,15)
-print("extendedGCD(25,10)-> ", "GCD:", gcd,"\ts:", s, "\tt:",t)
+run(54046,16802)
