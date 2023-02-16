58efa6b29fcb9c3aaca64b19fee7d33ac6916846
coremed <ben@aptlabs.net>
2023-02-16 17:49:23 -0500

    lfggg inital permutation working

--- des/des_encrypt.py
+++ des/des_encrypt.py
@@ -31,3 +31,39 @@ def xor(x, y):
     """
     xored = x ^ y
     return xored
+
+
+# Initial permutation 
+initial_permutation_table = (
+                [58, 50, 42, 34, 26, 18, 10, 2,
+                60, 52, 44, 36, 28, 20, 12, 4,
+                62, 54, 46, 38, 30, 22, 14, 6,
+                64, 56, 48, 40, 32, 24, 16, 8,
+                57, 49, 41, 33, 25, 17, 9, 1,
+                59, 51, 43, 35, 27, 19, 11, 3,
+                61, 53, 45, 37, 29, 21, 13, 5,
+                63, 55, 47, 39, 31, 23, 15, 7]
+)
+
+expansion_table = (
+    []
+)
+
+def main():
+    plaintext = input("Enter plaintext: ")
+    plaintext_hex = plaintext.encode("utf-8").hex()
+    print(f"Plaintext in base16: {plaintext_hex}")
+
+    # appends 0's to make the text 64bit
+    plaintext_bin = str(format(int(plaintext_hex, 16), "064b"))
+    print(f"Plaintext in base2: {plaintext_bin}")
+    plaintext_permuted = permutation(plaintext_bin, initial_permutation_table, 64)
+    print(f"Plaintext after inital permutation: {plaintext_permuted}")
+
+    # split step
+    left_pt = plaintext_permuted[0:32]
+    right_pt = plaintext_permuted[32:64]
+
+
+if __name__ == "__main__":
+    main()
